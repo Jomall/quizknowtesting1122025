0687b658f3500c0003dc75567f17547bab2dd461
User <user@example.com>
2025-12-07 13:49:09 -0400

    Update MongoDB URI to use correct username 'Jomall' matching Vercel config

--- test-db.js
+++ test-db.js
@@ -1,2 +1,2 @@
 =======
-    const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://quizknow-user:Jaedon532@quizknow-cluster.9iwzfgz.mongodb.net/quizknow';
+    const mongoUri = process.env.MONGODB_URI || 'mongodb+srv://Jomall:Jaedon532@quizknow-cluster.9iwzfgz.mongodb.net/quizknow';
