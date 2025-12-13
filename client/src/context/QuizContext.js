a1676fecd0fedddf0e13cb71a9a428350ca5f500
User <user@example.com>
2025-12-13 08:39:17 -0400

    Fix ESLint error: remove unused useMemo import from QuizContext.js

--- client/src/context/QuizContext.js
+++ client/src/context/QuizContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useContext, useReducer, useCallback, useMemo } from 'react';
+import React, { createContext, useContext, useReducer, useCallback } from 'react';
 import quizAPI from '../services/quizAPI';
 import axios from 'axios';
 
